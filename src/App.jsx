174263384f4734ff0484999dce52133b9870ad03
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-08-14 12:59:10 +0600

    add converter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {} from "react";
+import "./App.css";
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div>
+      <h1>Temperature-Scale</h1>
+      <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BoilingVerdict.jsx
@@ -0,0 +1,8 @@
+export default function BoilingVerdict({celsius = 0}){
+    if( celsius >= 100){
+        return <p>The water would boil.</p>
+    }
+    else{
+        return <p>water would not boil.</p>
+    }
+}
